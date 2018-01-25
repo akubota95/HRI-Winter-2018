cd1949206ead0d276929718db84f94a6339ee951
akubota <akubota@g.hmc.edu>
2018-01-25 14:02:11 -0800

    Compiling follower

---
+++ catkin_ws/build/follower_experiments/CMakeFiles/follower.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/follower.dir/src/hw5follower2.3.7.cpp.o"
+  "/home/turtlebot/Documents/HRI-akubota/catkin_ws/devel/lib/follower_experiments/follower.pdb"
+  "/home/turtlebot/Documents/HRI-akubota/catkin_ws/devel/lib/follower_experiments/follower"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/follower.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ catkin_ws/build/follower_experiments/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/turtlebot/Documents/HRI-akubota/catkin_ws/src/follower_experiments
+# Build directory: /home/turtlebot/Documents/HRI-akubota/catkin_ws/build/follower_experiments
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
